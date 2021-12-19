5ad99b51065c78111317309455cc781fc0ca502a
trsampson2 <86331812+trsampson2@users.noreply.github.com>
2021-12-19 11:10:20 -0500

    Added #2

--- MechaCarChallange.R
+++ MechaCarChallange.R
@@ -4,3 +4,6 @@ mechacar <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactor
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar))
 
+susp_coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- susp_coil %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance =var(PSI), SD = sd(PSI)) #create summary table with multiple columns 
+lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance = var(PSI), SD = sd(PSI)) # create summary table of the Manufacturing Lot
