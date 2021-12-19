7dd5382c56c65b8c282a642da863d9d935587d97
trsampson2 <86331812+trsampson2@users.noreply.github.com>
2021-12-19 12:21:34 -0500

    Final Submission

--- MechaCarChallange.R
+++ MechaCarChallange.R
@@ -1,13 +1,16 @@
 library(dplyr) #loads in the dplyr library
-mechacar <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors =F) #read in CSV to analyze
 
+#Deliverable #1
+mechacar <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors =F) #read in CSV to analyze
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar))
 
+#Deliverable #2
 susp_coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 total_summary <- susp_coil %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance =var(PSI), SD = sd(PSI)) #create summary table with multiple columns 
 lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance = var(PSI), SD = sd(PSI)) # create summary table of the Manufacturing Lot
 
+#Deliverable #3
 t.test(susp_coil$PSI, mu=1500) # t-test that compares all manufacturing lots
 
 lot1 <- susp_coil %>% filter(Manufacturing_Lot=="Lot1") # filters out manufacturing lot1
